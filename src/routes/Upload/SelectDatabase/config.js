fae9cd5bd4aa79ddda425bdf7b28a68c34443044
akamuinsaner <15108387504@163.com>
2018-09-27 08:52:55 +0800

    FIX 【同步客户端】最后3个类型，不需要有

--- src/routes/Upload/SelectDatabase/config.js
+++ src/routes/Upload/SelectDatabase/config.js
@@ -6,11 +6,5 @@ export const databaseConfig = [{
   name: 'Microsoft SQL', db_type: '2', source_type: '4', logo: null
 }, {
   name: 'Oracle', db_type: '3', source_type: '5', logo: null
-}, {
-  name: 'SQLite', db_type: '4', source_type: '7', logo: null
-}, {
-  name: 'SyBase', db_type: '5', source_type: '8', logo: null
-}, {
-  name: 'Firebird', db_type: '6', source_type: '9', logo: null
 }]
 export const example = []
