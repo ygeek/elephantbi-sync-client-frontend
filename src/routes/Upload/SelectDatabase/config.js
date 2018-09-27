9a232c55bbdb6cec67882021ffb93cb87cffb069
akamuinsaner <15108387504@163.com>
2018-09-27 14:55:08 +0800

    FIX icon替换

--- src/routes/Upload/SelectDatabase/config.js
+++ src/routes/Upload/SelectDatabase/config.js
@@ -1,10 +1,14 @@
+import postgresql from 'assets/postgreSQL.svg'
+import mysql from 'assets/mysql-logo.svg'
+import mssql from 'assets/microsoftsql-logo.svg'
+import oracle from 'assets/oracle.svg'
 export const databaseConfig = [{
-  name: 'MySQL', db_type: '0', source_type: '2', logo: null
+  name: 'MySQL', db_type: '0', source_type: '2', logo: mysql
 }, {
-  name: 'PostgreSQL', db_type: '1', source_type: '3', logo: null
+  name: 'PostgreSQL', db_type: '1', source_type: '3', logo: postgresql
 }, {
-  name: 'Microsoft SQL', db_type: '2', source_type: '4', logo: null
+  name: 'Microsoft SQL', db_type: '2', source_type: '4', logo: mssql
 }, {
-  name: 'Oracle', db_type: '3', source_type: '5', logo: null
+  name: 'Oracle', db_type: '3', source_type: '5', logo: oracle
 }]
 export const example = []
